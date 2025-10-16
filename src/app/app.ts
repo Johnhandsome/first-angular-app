367515ebd2c0d2ad9216ec1086d1a7a1d3f5f547
John <An3439201@gmail.com>
2025-10-16 13:23:48 +0700

    Add favicon.ico to the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
